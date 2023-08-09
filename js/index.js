2cd94811b95fc49f6a1182b76a9f21e20f43879e
mynewyear <natalia.iakovleva@hotmail.com>
2023-08-08 23:53:48 -0700

    add message form and button

--- js/index.js
+++ js/index.js
@@ -22,3 +22,33 @@ for (let i = 0; i < skills.length; i++) {
         skill.innerHTML= skills[i];
         skillList.appendChild(skill);
 }
+// store leave_message
+let messageForm = document.forms.leave_message;
+//Add an event listener
+messageForm[0].addEventListener('submit', function(event) {
+    //prevent the default refreshing
+    event.preventDefault() 
+    // Retrieve values
+    const usersName = messageForm.usersName.value;
+    const usersEmail = messageForm.usersEmail.value;
+    const usersMessage = messageForm.usersMessage.value;
+    console.log(userName + "\n" + userEmail + "\n" + userMessage); //log
+
+    let messageSection = document.getElementById("messages");
+    let messageList = messageSection.querySelector("ul");
+
+    let newMessage = document.createElement("li");
+    newMessage.innerHTML = '<a href="mailto:">' + event.target.name.value + '</a>' + ' <span>wrote: ' + event.target.message.value + '</span>  '
+    messageList.appendChild(newMessage);
+
+    const removeButton = document.createElement("button");
+    removeButton.type = "button";
+    removeButton.innerText = "Remove";
+    removeButton.addEventListener('click', function(e) {
+        let entry = removeButton.parentElement;
+        entry.remove();
+    });
+    newMessage.appendChild(removeButton);  
+    messageList.appendChild(newMessage);
+    messageForm.reset();
+});
